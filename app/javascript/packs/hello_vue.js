e41edbb91d30a69fa571672a21a952b45bd0e86a
Maria Famador <maria.famador87@gmail.com>
2020-01-03 10:35:24 +0200

    Comment out hello_vue.js

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -5,17 +5,17 @@
 // like app/views/layouts/application.html.erb.
 // All it does is render <div>Hello Vue</div> at the bottom of the page.
 
-import Vue from 'vue'
-import App from '../app.vue'
+// import Vue from 'vue'
+// import App from '../app.vue'
 
-document.addEventListener('DOMContentLoaded', () => {
-  const app = new Vue({
-    render: h => h(App)
-  }).$mount()
-  document.body.appendChild(app.$el)
+// document.addEventListener('DOMContentLoaded', () => {
+//   const app = new Vue({
+//     render: h => h(App)
+//   }).$mount()
+//   document.body.appendChild(app.$el)
 
-  console.log(app)
-})
+//   console.log(app)
+// })
 
 
 // The above code uses Vue without the compiler, which means you cannot
@@ -33,13 +33,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // import Vue from 'vue/dist/vue.esm'
-// import App from '../app.vue'
-//
+// import App from './components/app.vue'
+
 // document.addEventListener('DOMContentLoaded', () => {
 //   const app = new Vue({
 //     el: '#hello',
-//     data: {
-//       message: "Can you say hello?"
+//     data: function() {
+//       return { message: "Can you say hello?" }
 //     },
 //     components: { App }
 //   })
